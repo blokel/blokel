d0bf1148428260543b75a9357b0f6e76bd5ea7b2
devmarth <prestotron7@gmail.com>
2015-03-20 12:41:15 -0400

    Fix commits

---
+++ util/travis/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+if [ $WINDOWS = "no" ]; then
+	mkdir -p travisbuild
+	cd travisbuild
+	cmake -DENABLE_GETTEXT=1 -DENABLE_LEVELDB=1 -DENABLE_REDIS=1 -DCMAKE_BUILD_TYPE=Debug ..
+	make -j2
+	echo "Running unit tests for minetest"
+	../bin/minetest --run-unittests
+	echo "Running unit tests for minetestserver"
+	../bin/minetestserver --run-unittests
+else
+	[ $CC = "clang" ] && exit 1 # Not supposed to happen
+	# We need to have our build directory outside of the minetest directory because
+	#  CMake will otherwise get very very confused with symlinks and complain that
+	#  something is not a subdirectory of something even if it actually is.
+	# e.g.:
+	# /home/travis/minetest/minetest/travisbuild/minetest
+	# \/  \/  \/
+	# /home/travis/minetest/minetest/travisbuild/minetest/travisbuild/minetest
+	# \/  \/  \/
+	# /home/travis/minetest/minetest/travisbuild/minetest/travisbuild/minetest/travisbuild/minetest
+	# You get the idea.
+	OLDDIR=`pwd`
+	cd ..
+	[ $WINDOWS = "32" ] && EXISTING_MINETEST_DIR=$OLDDIR NO_MINETEST_GAME=1 $OLDDIR/util/buildbot/buildwin32.sh travisbuild && exit 0
+	[ $WINDOWS = "64" ] && EXISTING_MINETEST_DIR=$OLDDIR NO_MINETEST_GAME=1 $OLDDIR/util/buildbot/buildwin64.sh travisbuild && exit 0
+fi
